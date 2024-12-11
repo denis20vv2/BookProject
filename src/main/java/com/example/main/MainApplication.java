8dc8dfb93412b69879087bf6d500fc3283c2aa16
Safronov Denis <denis.safronov.228@gmail.com>
2024-12-11 16:54:09 +0300

    added starting service

--- src/main/java/com/example/main/MainApplication.java
+++ src/main/java/com/example/main/MainApplication.java
@@ -1,4 +1,25 @@
 package com.example.main;
 
+import com.example.shop.ShopApplication;
+import com.example.storage.StorageApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 public class MainApplication {
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(ShopApplication.class)
+                .properties("spring.config.name=application-shop") 
+                .properties("server.port=8081")
+                .run(args);
+
+
+        new SpringApplicationBuilder(StorageApplication.class)
+                .properties("spring.config.name=application-storage")
+                .properties("server.port=8080")
+                .run(args);
+    }
+
 }
+
